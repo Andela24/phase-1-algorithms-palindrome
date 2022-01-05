91261511551115e249357c4d3667f990f45316e2
Andela Stevanovic <andjela.s166@gmail.com>
2022-01-05 13:41:02 -0500

    commit tree 5ce918f21de2655f431bf7c734908e879b8e14ab

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
 function isPalindrome(word) {
   // Write your algorithm here
-}
+  let reversed = word.split('').reverse().join('');
+  console.log(reversed)
 
+  if(reversed === word)
+  return true;
+
+  return false
+}
+isPalindrome("robot")
 /* 
   Add your pseudocode here
+  make a function that returns true if a word is polindrome(in meaning if is 
+    reads the same forwards and backwards), or return false if not.
+  test case:
+  mom === mom //true
+  abc ===bca //false
 */
 
 /*
   Add written explanation of your solution here
+  doing .split() to convert an array of letters,
+  then .reverse()to reverse all the letters in the array
+  then .join() to convert back to an string
 */
 
 // You can run `node index.js` to view these console logs
